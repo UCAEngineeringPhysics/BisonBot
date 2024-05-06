90a8d89a5916806ac8c6915f5882006ffb4da02c
linzhanguca <lzhang12@ucae.edu>
2024-05-06 15:54:33 -0500

    update wheel controller. Start testing

--- bison_pico/wheel_controller.py
+++ bison_pico/wheel_controller.py
@@ -7,8 +7,8 @@ class WheelController:
         self.sabertooth_bridge = UART(0, baudrate=9600, tx=Pin(16), rx=Pin(17))
         self.ENCA_PIN = Pin(enca_id, Pin.IN, Pin.PULL_DOWN)
         self.ENCB_PIN = Pin(encb_id, Pin.IN, Pin.PULL_DOWN)
-        self.ENCA_PIN.irq(trigger=Pin.IRQ_FALLING|Pin.IRQ_RISING, handler=self.inc_counts_a)
-        self.ENCB_PIN.irq(trigger=Pin.IRQ_FALLING|Pin.IRQ_RISING, handler=self.inc_counts_b)
+        self.ENCA_PIN.irq(trigger=Pin.IRQ_FALLING|Pin.IRQ_RISING, handler=self.inc_counts)
+        # self.ENCB_PIN.irq(trigger=Pin.IRQ_FALLING|Pin.IRQ_RISING, handler=self.inc_counts)
         # Properties
         self.WHEEL_RADIUS = 0.127  # m, diameter is 10"
         self.GEAR_RATIO = 32
@@ -33,39 +33,36 @@ class WheelController:
         # self.controller_timer = Timer()
         # self.controller_timer.init(freq=100, callback=self.con_cb)
 
-    def inc_counts_a(self, pin):
+    def inc_counts(self, pin):
         self.enc_counts += self.wdir
 
-    def inc_counts_b(self, pin):
-        self.enc_counts += self.wdir
+    # def mon_cb(self, timer):
+    #     enc_diff = self.enc_counts - self.prev_enc_counts
+    #     omega_m = enc_diff / self.CPR * 2 * pi * 100 # angular velocity on motor shaft (rad / s)
+    #     omega_w = omega_m / self.GEAR_RATIO
+    #     self.lin_vel = omega_w * self.WHEEL_RADIUS
+    #     self.prev_enc_counts = self.enc_counts
 
-    def mon_cb(self, timer):
-        enc_diff = self.enc_counts - self.prev_enc_counts
-        omega_m = enc_diff / self.CPR * 2 * pi * 100 # angular velocity on motor shaft (rad / s)
-        omega_w = omega_m / self.GEAR_RATIO
-        self.lin_vel = omega_w * self.WHEEL_RADIUS
-        self.prev_enc_counts = self.enc_counts
-
-    def con_cb(self, timer):
-        self.err = self.ref_vel - self.lin_vel
-        self.err_sum += self.err  # err_sum = err_sum + err
-        self.err_diff = self.err - self.prev_err
-        self.prev_err = self.err
-        duty_inc = self.K_P * self.err + self.K_I * self.err_sum + self.K_D * self.err_diff  # Proportional, Integral, Derivative
-        self.duty += duty_inc
-        if self.duty > 0:  # forward
-            if self.duty >= 1:
-                self.duty = 0.999
-            self._forward(self.duty)
-        elif self.duty < 0:  # backward
-            if self.duty <= -1:
-                self.duty = -0.999
-            self._backward(-self.duty)
-        else:
-            self.stop()
-        if self.ref_vel == 0:
-            self.dc = 0.
-            self.stop()
+    # def con_cb(self, timer):
+    #     self.err = self.ref_vel - self.lin_vel
+    #     self.err_sum += self.err  # err_sum = err_sum + err
+    #     self.err_diff = self.err - self.prev_err
+    #     self.prev_err = self.err
+    #     duty_inc = self.K_P * self.err + self.K_I * self.err_sum + self.K_D * self.err_diff  # Proportional, Integral, Derivative
+    #     self.duty += duty_inc
+    #     if self.duty > 0:  # forward
+    #         if self.duty >= 1:
+    #             self.duty = 0.999
+    #         self._forward(self.duty)
+    #     elif self.duty < 0:  # backward
+    #         if self.duty <= -1:
+    #             self.duty = -0.999
+    #         self._backward(-self.duty)
+    #     else:
+    #         self.stop()
+    #     if self.ref_vel == 0:
+    #         self.dc = 0.
+    #         self.stop()
 
     def set_vel(self, target_vel):
         if target_vel is not self.ref_vel:
@@ -97,33 +94,37 @@ class WheelController:
 # TEST
 if __name__ == '__main__':
     from time import sleep
-    lwh = WheelController(10, 11)
-    rwh = WheelController(12, 13, motor_id='M2')
+    lwh = WheelController(2, 3)
+    rwh = WheelController(4, 5, motor_id='M2')
 
-    for d in range(100):
-        lwh._set_duty(duty=d/100)
-        rwh._set_duty(duty=d/100)
+    for d in range(40):
+        lwh._set_duty(duty=d/40)
+        # rwh._set_duty(duty=d/100)
         # print(lwh.enc_counts, rwh.enc_counts)
         # print(lwh.lin_vel, rwh.lin_vel)
-        sleep(0.04)
-    for d in reversed(range(100)):
-        lwh._set_duty(duty=d/100)
-        rwh._set_duty(duty=d/100)
+        sleep(0.1)
+    # print(lwh.enc_counts, rwh.enc_counts)
+    # for d in reversed(range(40)):
+        # lwh._set_duty(duty=d/40)
+        # rwh._set_duty(duty=d/100)
         # print(lwh.enc_counts, rwh.enc_counts)
         # print(lwh.lin_vel, rwh.lin_vel)
-        sleep(0.04)
-    for d in range(0, -100, -1):
-        lwh._set_duty(duty=d/100)
-        rwh._set_duty(duty=d/100)
+        # sleep(0.1)
+    # print(lwh.enc_counts, rwh.enc_counts)
+    # for d in range(0, -40, -1):
+        # lwh._set_duty(duty=d/40)
+        # rwh._set_duty(duty=d/100)
         # print(lwh.enc_counts, rwh.enc_counts)
         # print(lwh.lin_vel, rwh.lin_vel)
-        sleep(0.04)
-    for d in reversed(range(0, -100, -1)):
-        lwh._set_duty(duty=d/100)
-        rwh._set_duty(duty=d/100)
+        # sleep(0.1)
+    # print(lwh.enc_counts, rwh.enc_counts)
+    # for d in reversed(range(0, -40, -1)):
+    #     lwh._set_duty(duty=d/40)
+        # rwh._set_duty(duty=d/100)
         # print(lwh.enc_counts, rwh.enc_counts)
         # print(lwh.lin_vel, rwh.lin_vel)
-        sleep(0.04)
+    #     sleep(0.1)
+    # print(lwh.enc_counts, rwh.enc_counts)
 
     # lwh.set_vel(-0.44)
     # rwh.set_vel(-0.4)
