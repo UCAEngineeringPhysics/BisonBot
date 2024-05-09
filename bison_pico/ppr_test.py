5c8e3cd8ea00568cb8fe80811ff16fc3a9a0f14b
linzhanguca <lzhang12@ucae.edu>
2024-05-09 11:00:18 -0500

    update cpr estimation to ppr est.

--- bison_pico/ppr_test.py
+++ bison_pico/ppr_test.py
@@ -1,5 +1,5 @@
 """
-Use this script to verify encoder's CPR (~2048).
+Use this script to verify encoder's PPR (~1048).
 Parts:
     Quadrature Rotary Encoder: YUMO E6B2-CWZ3E (https://www.robotshop.com/products/6mm-rotary-encoder-1024-p-r)
     DC Motor (for electric wheelchair)
@@ -7,8 +7,7 @@ Wiring:
     Pico: Micro-USB         -->     Computer: USB
     Encoder: Brown (VCC)    -->     Pico: VBUS
     Encoder: Blue (GND)     -->     Pico: GND
-    Encoder: Black (A)      -->     Pico: GPIO10
-    Encoder: White (B)      -->     Pico: GPIO11
+    Encoder: Black (A)      -->     Pico: GPIO14
 Count encoder triggers:
     1. Run this script.
     2. Manually rotate the wheel and observe how many times the encoders are triggered.
@@ -16,17 +15,15 @@ Count encoder triggers:
 from machine import Pin
 
 # SETUP
-a_pin = Pin(10, Pin.IN, Pin.PULL_DOWN)
-b_pin = Pin(11, Pin.IN, Pin.PULL_DOWN)
+enc_pin = Pin(14, Pin.IN, Pin.PULL_DOWN)
 trig_counts = 0 
 def inc_counts(pin):
     global trig_counts
     trig_counts += 1
-a_pin.irq(trigger=Pin.IRQ_RISING, handler=inc_counts)
-b_pin.irq(trigger=Pin.IRQ_RISING, handler=inc_counts)
+enc_pin.irq(trigger=Pin.IRQ_RISING, handler=inc_counts)
 
 # LOOP
 while True:
     from time import sleep
     print(trig_counts)
-    sleep(0.01)
+    sleep(0.1)
